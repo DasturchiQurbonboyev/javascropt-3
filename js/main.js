9931c227a445d99c13d5d86cfe1a7284687806ba
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2023-12-07 09:53:47 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,174 @@
+// if 1
+
+// let a = +prompt("a ni kiriting ");
+
+// a > 0 ? a++ : console.log(a);
+
+// console.log(a);
+
+
+// if 2
+
+// let a = +prompt("a ni kiriting ");
+// let b = +prompt("b ni kiriting ");
+// let c = +prompt("c ni kiriting ");
+
+
+// if (a>b){
+//     if (a>c) {
+//         if (b>c){
+//             console.log(a);            
+//             console.log(b);            
+//         } else {
+//             console.log(a);
+//             console.log(c);
+//         }
+//     } else {
+//         console.log(a);
+//         console.log(c);
+//     }
+// } else {
+//     if (b>c) {
+//         if (a>c) {
+//             console.log(b);
+//             console.log(a);
+//         }else {
+//             console.log(b);
+//             console.log(c);
+//         }
+//     } else {
+//         console.log(c);
+//         console.log(b);
+//     }
+// }
+
+// if 3
+
+// let A = +prompt("A ni kiriting ");
+// let B = +prompt("B ni kiriting ");
+// let C = +prompt("C ni kiriting ");
+
+// if (A<B){
+//     if (B<C){
+//         console.log(A*2);
+//         console.log(B*2);
+//         console.log(C*2);
+//     } else {
+//         console.log(A*(-1));
+//         console.log(B*(-1));
+//         console.log(C*(-1));
+//     }
+// }else{
+//     console.log(A*(-1));
+//     console.log(B*(-1));
+//     console.log(C*(-1));
+// }
+
+// if 4
+
+// let A = +prompt("A ni kiriting ");
+// let B = +prompt("B ni kiriting ");
+// let C = +prompt("C ni kiriting ");
+
+// if (A>B){
+//     if (B>C){
+//         console.log(A*2);
+//         console.log(B*2);
+//         console.log(C*2);
+//     } else {
+//         if (C>A) {
+//             console.log(A*(-1));
+//             console.log(B*(-1));
+//             console.log(C*(-1));
+//         }else{
+//             console.log(A*(-1));
+//             console.log(B*(-1));
+//             console.log(C*(-1));
+//         }
+//     }
+    
+// }else {
+//     if (C>B){
+//         console.log(A*2);
+//         console.log(B*2);
+//         console.log(C*2);
+//     }else{
+//         console.log(A*(-1));
+//         console.log(B*(-1));
+//         console.log(C*(-1));
+//         }
+// }
+
+
+
+// if 5
+
+let a = +prompt("a ni kiriting ");
+let b = +prompt("b ni kiriting ");
+let c = +prompt("c ni kiriting ");
+let d = +prompt("d ni kiriting ");
+
+if (a==b){
+    if (b==c){
+        console.log(4);
+    }else{
+        if (b==d){
+            console.log(3);
+        }
+    }
+
+}else {
+    if (a==c)
+}
+
+// if 6
+
+
+
+// if 7
+
+
+
+// if 8
+
+
+
+// if 9
+
+
+
+// if 10
+
+
+
+// if 11
+
+
+
+// if 12
+
+
+
+// if 13
+
+
+
+// if 14
+
+
+
+// if 15
+
+
+
+// if 16
+
+
+
+// if 17
+
+
+
+// if 18
+
+
